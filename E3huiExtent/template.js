df5149124fb796ba0caedf4ef6407e24c4986b09
wangxdsh <wangxdsh@163.com>
2025-11-23 15:56:21 +0800

    修改按钮

--- E3huiExtent/template.js
+++ E3huiExtent/template.js
@@ -8,16 +8,18 @@ window.widgetTemplate = `
     <!-- 窗口内容区 -->
     <div class="widget-content">
       <!-- 截图按钮 -->
-      <button class="screenshot-btn">点击截取商品左侧信息区</button>
-      <!-- 加入产品库按钮（默认隐藏） -->
-      <button class="add-to-library-btn">加入产品库</button>
-      <!-- 新增：提交产品库 Loading（悬浮窗口内，默认隐藏） -->
-      <div class="submit-loading" style="display: none;">
-        <span class="loading-spinner"></span>
-        <span class="loading-text">提交中...</span>
-      </div>
-      <!-- 截图加载中提示 -->
-      <div class="widget-loading">截图中...</div>
+      <div class ="btn-content">
+        <button class="screenshot-btn">获取素材</button>
+        <!-- 加入产品库按钮（默认隐藏） -->
+        <button class="add-to-library-btn">加入产品库</button>
+        <!-- 新增：提交产品库 Loading（悬浮窗口内，默认隐藏） -->
+        <div class="submit-loading" style="display: none;">
+          <span class="loading-spinner"></span>
+          <span class="loading-text">提交中...</span>
+        </div>
+        <!-- 截图加载中提示 -->
+        <div class="widget-loading">截图中...</div>
+      </div>  
       <!-- 截图预览（默认隐藏） -->
       <div class="screenshot-preview" style="display: none;">
         <h4 class="list-title">截图预览</h4>
